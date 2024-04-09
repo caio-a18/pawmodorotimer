87b0de09c26bfb38abea67f45a29c70ae61824ac
Asya <aya29@case.edu>
2024-04-09 17:45:10 -0400

    server.js reconfigured bc it doesnt work otherwise ahaha ahaha

--- public/server.js
+++ public/server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+
+/*const express = require('express');
 const cors = require('cors');
 const app = express();
 
@@ -71,3 +72,18 @@ app.post('/api/user/level/update/:userId', (req, res) => {
 app.listen(8080, () => console.log('API is running on http://localhost:8080'));
 
 });
+*/
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'test123'
+  });
+});
+
+app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
