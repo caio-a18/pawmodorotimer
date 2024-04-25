40c35ccf24dc44b3734498c25a213e8e8acd2ec1
Asya <aya29@case.edu>
2024-04-24 22:05:35 -0400

    test cleanup

--- frontend/src/AppTest.test.js
+++ frontend/src/AppTest.test.js
@@ -44,7 +44,7 @@ describe('Login Component', () => {
 //test area for app component
   describe('App Component', () => {
 
-    //check for rendering 60 mins button 
+    /****check for rendering****/
     it('renders the 60 Minutes button', () => {
       const { getByText } = render(<App />);
       const sixtyMinutesButton = getByText(/60 Minutes/i);
@@ -63,78 +63,48 @@ describe('Login Component', () => {
       expect(fiveMinutesButton).toBeDefined();
     });
 
+    /****check that initial time rendered is accurate****/
     it('should render the timer component with initial time of 0:00', () => {
       const { getByTestId } = render(<App />);
       const timerElement = getByTestId('timer');
       expect(timerElement).toHaveTextContent('0:00');
     });
 
+    /****timers start counting down when proper button is pressed****/
     it('Timer starts countdown when 60 minutes selected', () => {
-      // Render the component
     const { getByText, getByTestId } = render(<App />);
-  
-    // Find the button for 60 Minutes and click it
     const button60Minutes = getByText('60 Minutes');
     fireEvent.click(button60Minutes);
-
     jest.advanceTimersByTime(1000);
   
-    // Wait for the timer to start counting down
      waitFor(() => {
-      expect(getByTestId('timer')).toHaveTextContent('59:59'); // Assuming the timer starts at 60 minutes and counts down
+      expect(getByTestId('timer')).toHaveTextContent('59:59'); 
     });
     });
+
+    it('Timer starts countdown when 20 minutes selected', () => {
+      const { getByText, getByTestId } = render(<App />);
+      const button20Minutes = getByText('20 Minutes');
+      fireEvent.click(button20Minutes);
+      jest.advanceTimersByTime(1000);
+    
+       waitFor(() => {
+        expect(getByTestId('timer')).toHaveTextContent('19:59'); 
+      });
+      });
+
+      it('Timer starts countdown when 5 minutes selected', () => {
+        const { getByText, getByTestId } = render(<App />);
+        const button5Minutes = getByText('5 Minutes');
+        fireEvent.click(button5Minutes);
+        jest.advanceTimersByTime(1000);
+      
+         waitFor(() => {
+          expect(getByTestId('timer')).toHaveTextContent('4:59'); 
+        });
+        });
   
   });
 
-  test('Timer starts countdown when 60 Minutes button is clicked', async () => {
-    // Render the component
-    const { getByText, getByTestId } = render(<App />);
+ 
   
-    // Find the button for 60 Minutes and click it
-    const button60Minutes = getByText('60 Minutes');
-    fireEvent.click(button60Minutes);
-
-    jest.advanceTimersByTime(1000);
-  
-    // Wait for the timer to start counting down
-    await waitFor(() => {
-      expect(getByTestId('timer')).toHaveTextContent('59:59'); // Assuming the timer starts at 60 minutes and counts down
-    });
-  });
-
-  test('Timer starts countdown when 20 Minutes button is clicked', async () => {
-    // Render the component
-    const { getByText, getByTestId } = render(<App />);
-  
-    // Find the button for 20 Minutes and click it
-    const button20Minutes = getByText('20 Minutes');
-    fireEvent.click(button20Minutes);
-
-    jest.advanceTimersByTime(500);
-    jest.advanceTimersByTime(500); 
-    // Wait for the timer to start counting down
-    await waitFor(() => {
-      expect(getByTestId('timer')).toHaveTextContent('19:59'); 
-    });
-  });
-
-  test('Timer starts countdown when 5 Minutes button is clicked', async () => {
-    // Render the component
-    const { getByText, getByTestId } = render(<App />);
-  
-    // Find the button for 20 Minutes and click it
-    const button5Minutes = getByText('5 Minutes');
-    fireEvent.click(button5Minutes);
-
-    jest.advanceTimersByTime(500);
-    jest.advanceTimersByTime(500);
-    // Wait for the timer to start counting down
-    await waitFor(() => {
-      expect(getByTestId('timer')).toHaveTextContent('4:59'); 
-    });
-  });
-
-
-
-
